fe3d95e2ceaff569536d35f501f40dcef166a01a
Iury Cardoso <iury@iepro.com.br>
2021-08-22 02:10:26 -0300

    ✨ - Adicionada criacao de salas

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,9 @@
-import { ButtonHTMLAttributes } from "react";
+import { ButtonHTMLAttributes } from 'react'
 
-import "../styles/button.scss";
+import '../styles/button.scss'
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
 
 export function Button(props: ButtonProps) {
-  return <button className="button" {...props}></button>;
+  return <button className="button" {...props}></button>
 }
